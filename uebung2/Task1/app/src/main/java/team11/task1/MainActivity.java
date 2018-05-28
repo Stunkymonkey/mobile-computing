e3c148d11d1535e0e959349ab24a9a1ec808c884
Simon Hagenmayer <st107469@stud.uni-stuttgart.de>
2018-05-28 14:59:56 +0200

    Style

--- uebung2/Task1/app/src/main/java/team11/task1/MainActivity.java
+++ uebung2/Task1/app/src/main/java/team11/task1/MainActivity.java
@@ -18,7 +18,6 @@ import android.widget.ScrollView;
 import android.widget.SeekBar;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.UUID;
 
@@ -29,8 +28,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BluetoothAdapter mBluetoothAdapter;
     private String uuid = "8e2e2964-e2f2-4d7e-a128-3e9f03ef6de7";
     private Button btn_Scan;
-    private Button btn_temp;
-    private Button btn_hum;
     private Scanner_BTLE mBTLeScanner;
     private HashMap<String, BTLE_Device> mBTDevicesHashMap;
     private ArrayList<BTLE_Device> mBTDevicesArrayList;
@@ -39,26 +36,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BroadcastReceiver_BTState mBTStateUpdateReceiver;
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
     private BluetoothGatt mGatt;
-    private SeekBar seek_bar;
     private boolean read_temp;
     private final String T_UUID = "00002A1C-0000-1000-8000-00805F9B34FB";
     private final String H_UUID = "00002A6F-0000-1000-8000-00805F9B34FB";
     private final String I_UUID = "10000001-0000-0000-FDFD-FDFDFDFDFDFD";
     private final String FAN_UUID = "00000001-0000-0000-FDFD-FDFDFDFDFDFD";
-
-    private float getTemperatureFromBytes(byte[] bytes) {
-        int mantisse = (((int) bytes[3]) << 16) + (((int) bytes[2]) << 8)  + ((int) bytes[1]);
-        float exponent = (float) bytes[4];
-        float value = (float) (mantisse * Math.pow(10.0, exponent));
-        return value;
-    }
-
-    private float getHumidityFromBytes(byte[] bytes) {
-        float value = (((int) bytes[1]) << 8) + ((int) bytes[0]);
-        return value / 100.0f;
-    }
-
-
     private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
@@ -68,23 +50,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         @Override
-        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-
-        }
-
-        @Override
         public void onCharacteristicRead(BluetoothGatt gatt,
                                          BluetoothGattCharacteristic characteristic,
                                          int status) {
-
             if (read_temp) {
                 float value = getTemperatureFromBytes(characteristic.getValue());
                 Log.i(LOG_TAG, String.valueOf(value));
-                ((Button)findViewById(R.id.button_temp)).setText("Temp: "+String.valueOf(value)+"°C");
+                ((Button)findViewById(R.id.button_temp)).setText("Temperature: "+String.valueOf(value)+"°C");
             } else {
                 float value = getHumidityFromBytes(characteristic.getValue());
                 Log.i(LOG_TAG, String.valueOf(value));
-                ((Button)findViewById(R.id.button_hum)).setText("Hum: "+String.valueOf(value)+"%");
+                ((Button)findViewById(R.id.button_hum)).setText("Humidity: "+String.valueOf(value)+"%");
             }
         }
 
@@ -104,7 +80,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
 
-
         @Override
         public void onCharacteristicWrite (BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             Log.i(LOG_TAG, "Writing characteristic: "+characteristic.getUuid());
@@ -114,13 +89,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public MainActivity() {
     }
 
+    private float getTemperatureFromBytes(byte[] bytes) {
+        int mantisse = (((int) bytes[3]) << 16) + (((int) bytes[2]) << 8)  + ((int) bytes[1]);
+        float exponent = (float) bytes[4];
+        float value = (float) (mantisse * Math.pow(10.0, exponent));
+        return value;
+    }
+
+    private float getHumidityFromBytes(byte[] bytes) {
+        float value = (((int) bytes[1]) << 8) + ((int) bytes[0]);
+        return value / 100.0f;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
-            //Utils.toast(getApplicationContext(), "BLE not supported");
+            //BLE not supported
             finish();
         }
 
@@ -140,25 +127,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btn_Scan = (Button) findViewById(R.id.btn_scan);
         ((ScrollView) findViewById(R.id.scrollView)).addView(listView);
-        findViewById(R.id.btn_scan).setOnClickListener(this);
-
-        btn_temp = (Button) findViewById(R.id.button_temp);
+        ((Button) findViewById(R.id.btn_scan)).setOnClickListener(this);
         ((Button) findViewById(R.id.button_temp)).setOnClickListener(this);
-
-        btn_hum = (Button) findViewById(R.id.button_hum);
         ((Button) findViewById(R.id.button_hum)).setOnClickListener(this);
-
-        seek_bar = (SeekBar) findViewById(R.id.seekBar1);
         ((SeekBar) findViewById(R.id.seekBar1)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                //Log.i(LOG_TAG, String.valueOf(progress));
-            }
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {}
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
@@ -170,6 +147,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        // Location permission
         if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             final AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("This app needs location access");
@@ -183,33 +161,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             });
             builder.show();
         }
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
         registerReceiver(mBTStateUpdateReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
         stopScan();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-
         unregisterReceiver(mBTStateUpdateReceiver);
         stopScan();
     }
@@ -221,26 +194,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        // Check which request we're responding to
+        // Bluetooth request
         if (requestCode == REQUEST_ENABLE_BT) {
-            // Make sure the request was successful
             if (resultCode == RESULT_OK) {
-                // Utils.toast(getApplicationContext(), "Thank you for turning on Bluetooth");
             } else if (resultCode == RESULT_CANCELED) {
                 Utils.toast(getApplicationContext(), "Please turn on Bluetooth");
             }
         }
     }
 
-    /**
-     * Adds a device to the ArrayList and Hashmap that the ListAdapter is keeping track of.
-     *
-     * @param device the BluetoothDevice to be added
-     * @param rssi   the rssi of the BluetoothDevice
-     */
     public void addDevice(BluetoothDevice device, int rssi) {
-
         String address = device.getAddress();
         if (!mBTDevicesHashMap.containsKey(address)) {
             BTLE_Device btleDevice = new BTLE_Device(device);
@@ -251,42 +214,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         } else {
             mBTDevicesHashMap.get(address).setRSSI(rssi);
         }
-
         adapter.notifyDataSetChanged();
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //Utils.toast(getApplicationContext(), "Item clicked at "+position+" "+id);
         Log.i(LOG_TAG, String.valueOf(view.getId()));
         BTLE_Device device = mBTDevicesArrayList.get(position);
-        //Utils.toast(getApplicationContext(), device.getName());
         mGatt = device.getBluetoothDevice().connectGatt(getApplicationContext(), false,
                 mGattCallback);
-        /*
-        if (mGattCharacteristics != null) {
-            final BluetoothGattCharacteristic characteristic =
-                    mGattCharacteristics.get(position);
-            if (mBTLeScanner.getWEATHER_UUID().equals(characteristic.getUuid())) {
-            }
-        }
-
-        Log.i(LOG_TAG, mGattCharacteristics.toString());
-        */
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_scan:
-                //Utils.toast(getApplicationContext(), "Scan Button Pressed");
-
                 if (!mBTLeScanner.isScanning()) {
                     startScan();
                 } else {
                     stopScan();
                 }
-
                 break;
             case R.id.button_temp:
                 read_temp = true;
@@ -319,24 +266,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void startScan() {
         btn_Scan.setText("Scanning...");
-
         mBTDevicesArrayList.clear();
         mBTDevicesHashMap.clear();
         mGattCharacteristics.clear();
-
-
         adapter.notifyDataSetChanged();
-
         mBTLeScanner.start();
     }
 
-    /**
-     * Stops Scanner_BTLE
-     * Changes the scan button text.
-     */
     public void stopScan() {
         btn_Scan.setText("Scan Again");
-
         mBTLeScanner.stop();
     }
 }
